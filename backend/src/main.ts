f8b3620cefdd7046be665c7975f29251ce4d4527
GustavoVianaN <86171973+GustavoVianaN@users.noreply.github.com>
2024-03-08 13:35:29 -0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,13 +7,16 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 async function bootstrap() {
 
   const httpsOptions = {
-    key: fs.readFileSync('certs/ipms.privkey.pem'),
-    cert: fs.readFileSync('certs/ipms.fullchain.pem'),
+    key: fs.readFileSync('certs/chave.privkey.pem'),
+    cert: fs.readFileSync('certs/chave.fullchain.pem'),
   };
 
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
+  // se quiser usar no https ative aqui
+  //const app = await NestFactory.create(AppModule, {
+  //  httpsOptions,
+  //});
+
+  const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('Project Watch Brasil API')
